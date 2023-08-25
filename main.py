daf961523a38624eb16a2f6bd7540925190f0e8f
dobravaza <grzegorz00.git@gmail.com>
2023-08-25 13:06:31 +0200

    ostatnie zmiany...

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
+import tkinter
+from PIL import Image, ImageTk
+import random
+import os
+
+root = tkinter.Tk()
+root.geometry('400x400')
+root.title('Roll the dice by @dobravaza')
+
+BlankLine = tkinter.Label(root, text="")
+BlankLine.pack()
+
+HeadingLabel = tkinter.Label(root, text="@dobravaza",
+   fg = "light green",
+     bg = "dark green",
+     font = "Helvetica 16 bold italic")
+
+HeadingLabel.pack()
+
+dice = ['die1.PNG', 'die2.PNG', 'die3.PNG', 
+    'die4.PNG', 'die5.PNG', 'die6.PNG']
+
+ImageLabel = tkinter.Label(root)
+ImageLabel.pack(expand=True)
+
+def rolling_dice():
+    DiceImage = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    ImageLabel.configure(image=DiceImage)
+    ImageLabel.image = DiceImage
+
+button = tkinter.Button(root, text="roll the dice", command=rolling_dice)
+button.pack()
+
+root.mainloop()
